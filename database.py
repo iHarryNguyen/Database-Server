9dc34eca72d1677d2d3da43af4b5a8f169ad0f20
Hnguyen18@worcester.edu <hnguyen18@worcester.edu>
2018-11-28 21:40:50 -0500

    Python server-side

--- database.py
+++ database.py
@@ -2,7 +2,8 @@
 import sqlite3
 
 #Where our data is going to be stored
-db = sqlite3.connect('database.db')
+db = sqlite3.connect('weatherStations.db')
+cur = db.cursor()
 #If we create another table 'Person' it will be ignored
 db.execute('DROP TABLE IF EXISTS Student')
  #Create Table 'Person' with said attributes
@@ -13,5 +14,8 @@ db.execute("""CREATE TABLE Student(
 
 db.execute("INSERT INTO Student VALUES('Harry', 'Nguyen', 25)")
 db.commit()
-db.execute("SELECT * FROM Student WHERE secondname = 'Nguyen'")
-print(fetchall())
+db.execute('DROP TABLE Student')
+db.commit()
+#print(cur.execute("SELECT * FROM Student WHERE secondname = 'Nguyen'"))
+#db.execute("DELETE * FROM Student")
+#db.commit()
