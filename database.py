82c1b4c626dbe3b1f65751472513dd81b4f21aa8
Hnguyen18@worcester.edu <hnguyen18@worcester.edu>
2018-11-29 19:29:07 -0500

    Queries and py files

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ import sqlite3
 
 #Where our data is going to be stored
 db = sqlite3.connect('weatherStations.db')
-cur = db.cursor()
 #If we create another table 'Person' it will be ignored
 db.execute('DROP TABLE IF EXISTS Student')
  #Create Table 'Person' with said attributes
@@ -14,8 +13,16 @@ db.execute("""CREATE TABLE Student(
 
 db.execute("INSERT INTO Student VALUES('Harry', 'Nguyen', 25)")
 db.commit()
-db.execute('DROP TABLE Student')
+
+#Adds column Year to Student Table and the input would be a text ie:Freshman, Sophomore, etc.
+db.execute("ALTER TABLE Student ADD Year text")
 db.commit()
+
+db.execute("UPDATE Student SET Year = 'Senior' WHERE firstname = 'Harry'")
+db.commit()
+#db.execute('DROP TABLE Student')
+#db.commit()
+
 #print(cur.execute("SELECT * FROM Student WHERE secondname = 'Nguyen'"))
 #db.execute("DELETE * FROM Student")
 #db.commit()
